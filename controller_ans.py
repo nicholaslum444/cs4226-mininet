1a5f08ce10f1f5b43b906d6185f9794748c58684
Nicholas Lum <nicholaslum444@hotmail.com>
2016-11-14 12:01:05 -0800

    completed task 2

--- controller_ans.py
+++ controller_ans.py
@@ -1,208 +1,106 @@
-# Copyright 2011 James McCauley
-#
-# This file is part of POX.
-#
-# POX is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# POX is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with POX.  If not, see <http://www.gnu.org/licenses/>.
+'''
+Please add your name: Nicholas Lum Aik Yong
+Please add your matric number: A0108358B
+'''
 
-"""
-An L2 learning switch.
-
-It is derived from one written live for an SDN crash course.
-It is somwhat similar to NOX's pyswitch in that it installs
-exact-match rules for each flow.
-"""
+import sys
+import os
+from sets import Set
 
 from pox.core import core
+
 import pox.openflow.libopenflow_01 as of
+import pox.openflow.discovery
+import pox.openflow.spanning_tree
+
+from pox.lib.revent import *
 from pox.lib.util import dpid_to_str
-from pox.lib.util import str_to_bool
-import time
+from pox.lib.addresses import IPAddr, EthAddr
 
 log = core.getLogger()
 
-# We don't want to flood immediately when a switch connects.
-# Can be overriden on commandline.
-_flood_delay = 0
-
-class LearningSwitch (object):
-    """
-    The learning switch "brain" associated with a single OpenFlow switch.
-
-    When we see a packet, we'd like to output it on a port which will
-    eventually lead to the destination.  To accomplish this, we build a
-    table that maps addresses to ports.
-
-    We populate the table by observing traffic.  When we see a packet
-    from some source coming from some port, we know that source is out
-    that port.
-
-    When we want to forward traffic, we look up the desintation in our
-    table.  If we don't know the port, we simply send the message out
-    all ports except the one it came in on.  (In the presence of loops,
-    this is bad!).
-
-    In short, our algorithm looks like this:
-
-    For each packet from the switch:
-    1) Use source address and switch port to update address/port table
-    2) Is transparent = False and either Ethertype is LLDP or the packet's
-       destination address is a Bridge Filtered address?
-       Yes:
-          2a) Drop packet -- don't forward link-local traffic (LLDP, 802.1x)
-              DONE
-    3) Is destination multicast?
-       Yes:
-          3a) Flood the packet
-              DONE
-    4) Port for destination address in our address/port table?
-       No:
-          4a) Flood the packet
-              DONE
-    5) Is output port the same as input port?
-       Yes:
-          5a) Drop packet and similar ones for a while
-    6) Install flow table entry in the switch so that this
-       flow goes out the appopriate port
-       6a) Send the packet out appropriate port
-    """
-    def __init__ (self, connection, transparent):
-        # Switch we'll be adding L2 learning switch capabilities to
-        self.connection = connection
-        self.transparent = transparent
-
-        # Our table
-        self.macToPort = {}
-
-        # We want to hear PacketIn messages, so we listen
-        # to the connection
-        connection.addListeners(self)
-
-        # We just use this to know when to log a helpful message
-        log.debug("flood delay: %i", _flood_delay)
-        self.hold_down_expired = _flood_delay == 0
-
-        #log.debug("Initializing LearningSwitch, transparent=%s",
-        #          str(self.transparent))
+class Controller(EventMixin):
+    def __init__(self):
+        self.listenTo(core.openflow)
+        core.openflow_discovery.addListeners(self)
+        # macmap is a 2d map, each switch has its own mac mapping
+        self.macmap = {}
 
     def _handle_PacketIn (self, event):
-        """
-        Handle packet in messages from the switch to implement above algorithm.
-        """
-
         packet = event.parsed
-        log.debug("%i: Packet %s", event.dpid, packet)
+        dpid = event.dpid
+        src = packet.src
+        dst = packet.dst
+        inport = event.port
 
-        def flood (message = None):
-            """ Floods the packet """
-            log.debug("flooding from %i", event.dpid);
-            msg = of.ofp_packet_out()
-            if time.time() - self.connection.connect_time >= _flood_delay:
-                # Only flood if we've been connected for a little while...
-
-                if self.hold_down_expired is False:
-                    # Oh yes it is!
-                    self.hold_down_expired = True
-                    log.info("%s: Flood hold-down expired -- flooding",
-                        dpid_to_str(event.dpid))
-
-                if message is not None: log.debug(message)
-                log.debug("%i: flood %s -> %s", event.dpid,packet.src,packet.dst)
-                # OFPP_FLOOD is optional; on some switches you may need to change
-                # this to OFPP_ALL.
-                msg.actions.append(of.ofp_action_output(port = of.OFPP_FLOOD))
-            else:
-                log.info("Holding down flood for %s", dpid_to_str(event.dpid))
+	# install entries to the route table
+        def install_enqueue(event, packet, outport, q_id = None):
+            log.debug("# S%i: Installing flow %s.%i -> %s.%i", dpid, src, inport, dst, outport)
+            msg = of.ofp_flow_mod()
+            msg.match = of.ofp_match.from_packet(packet, inport)
+            msg.actions.append(of.ofp_action_output(port = outport))
             msg.data = event.ofp
-            msg.in_port = event.port
-            self.connection.send(msg)
+            event.connection.send(msg)
+            log.debug("# S%i: Message sent via port %i\n", dpid, outport)
+            return
 
-        def drop (duration = None):
-            """
-            Drops this packet and optionally installs a flow to continue
-            dropping similar ones for a while
-            """
-            log.debug("%i: Packet has been dropped", event.dpid)
-            if duration is not None:
-                if not isinstance(duration, tuple):
-                    duration = (duration,duration)
-                msg = of.ofp_flow_mod()
-                msg.match = of.ofp_match.from_packet(packet)
-                msg.idle_timeout = duration[0]
-                msg.hard_timeout = duration[1]
-                msg.buffer_id = event.ofp.buffer_id
-                self.connection.send(msg)
-            elif event.ofp.buffer_id is not None:
-                msg = of.ofp_packet_out()
-                msg.buffer_id = event.ofp.buffer_id
-                msg.in_port = event.port
-                self.connection.send(msg)
+	# Check the packet and decide how to route the packet
+        def forward(message = None):
+            log.debug("# S%i: Recv %s via port %i", dpid, packet, inport)
 
-        self.macToPort[packet.src] = event.port # 1
+            # Store the incoming port
+            self.macmap[dpid][src] = inport
+            #print("# S%i: %s" % (dpid, self.macmap))
 
-        #if not self.transparent: # 2
-        #    if packet.type == packet.LLDP_TYPE or packet.dst.isBridgeFiltered():
-        #        drop() # 2a
-        #        return
+            # If multicast, flood
+            if dst.is_multicast:
+                flood("# S%i: Multicast to %s -- flooding" % (dpid, dst))
+                return
 
-        if packet.dst.is_multicast:
-            log.debug("%i: Multicast", event.dpid)
-            flood() # 3a
-        else:
-            if packet.dst not in self.macToPort: # 4
-                flood("Port for %s unknown -- flooding" % (packet.dst,)) # 4a
-            else:
-                port = self.macToPort[packet.dst]
-                if port == event.port: # 5
-                    # 5a
-                    log.warning("Same port for packet from %s -> %s on %s.%s.  Drop."
-                        % (packet.src, packet.dst, dpid_to_str(event.dpid), port))
-                    drop(10)
-                    return
-                # 6
-                log.debug("%i: installing flow for %s.%i -> %s.%i" %
-                          (event.dpid, packet.src, event.port, packet.dst, port))
-                msg = of.ofp_flow_mod()
-                msg.match = of.ofp_match.from_packet(packet, event.port)
-                msg.idle_timeout = 10
-                msg.hard_timeout = 30
-                msg.actions.append(of.ofp_action_output(port = port))
-                msg.data = event.ofp # 6a
-                self.connection.send(msg)
+            # If dst port not found, flood
+            if dst not in self.macmap[dpid]:
+                flood("# S%i: Port for %s unknown -- flooding" % (dpid, dst))
+                return
+
+            # get port and send out
+            outport = self.macmap[dpid][dst]
+            install_enqueue(event, packet, outport, 0)
+            return
+
+        # When it knows nothing about the destination, flood but don't install the rule
+        def flood (message = None):
+            log.debug(message)
+            msg = of.ofp_packet_out()
+            msg.actions.append(of.ofp_action_output(port = of.OFPP_FLOOD))
+            msg.data = event.ofp
+            msg.in_port = inport
+            event.connection.send(msg)
+            log.debug("# S%i: Message sent via port %i\n", dpid, of.OFPP_FLOOD)
+            return
+
+        # Ensure own macmap is initialised
+        if dpid not in self.macmap: self.macmap[dpid] = {}
+
+        # Begin
+        forward()
 
 
-class l2_learning (object):
-    """
-    Waits for OpenFlow switches to connect and makes them learning switches.
-    """
-    def __init__ (self, transparent):
-        core.openflow.addListeners(self)
-        self.transparent = transparent
+    def _handle_ConnectionUp(self, event):
+        dpid = dpid_to_str(event.dpid)
+        log.debug("Switch %s has come up.", dpid)
 
-    def _handle_ConnectionUp (self, event):
-        log.debug("Connection %s" % (event.connection,))
-        LearningSwitch(event.connection, self.transparent)
+	# Send the firewall policies to the switch
+        def sendFirewallPolicy(connection, policy):
+            pass
 
 
-def launch (transparent=False, hold_down=_flood_delay):
-    """
-    Starts an L2 learning switch.
-    """
-    try:
-        global _flood_delay
-        _flood_delay = int(str(hold_down), 10)
-        assert _flood_delay >= 0
-    except:
-        raise RuntimeError("Expected hold-down to be a number")
+        #for i in firewall policies:
+        #    sendFirewallPolicy(event.connection, i)
 
-    core.registerNew(l2_learning, str_to_bool(transparent))
+def launch():
+    # Run discovery and spanning tree modules
+    pox.openflow.discovery.launch()
+    pox.openflow.spanning_tree.launch()
+
+    # Starting the controller module
+    core.registerNew(Controller)
